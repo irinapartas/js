a4a5c5a28bc1c8ba5e263868e25422ea17924d6e
Ira <ilovebellsdaya@yandex.ru>
2020-05-22 23:25:58 +0300

    string

--- app.js
+++ app.js
@@ -1,65 +1,48 @@
-// 1 Number
-// const num = 42 // integer
-// const float = 42.42 // float
-// const pow = 10e3
+// const name = 'Ira'
+// const age = 19
+//
+// function getAge() {
+//   return age
+// }
 
-// console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
-// console.log('Math.pow 53', Math.pow(2, 53) - 1)
-// console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)
-// console.log('MAX_VALUE', Number.MAX_VALUE)
-// console.log('MIN_VALUE', Number.MIN_VALUE)
-// console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
-// console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY)
-// console.log('2 / 0', 2 / 0)
-// console.log(Number.NaN) // Not a number
-// console.log(typeof NaN)
-// const weird = 2 / undefined
-// console.log(Number.isNaN(weird))
-// console.log(Number.isNaN(42))
-// console.log(Number.isFinite(Infinity))
-// console.log(Number.isFinite(42))
+// const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет.'
+// const output = `Привет, меня зовут ${name} и мой возраст ${age < 20 ? 'A' : 'B'} лет.`
+// const output = `
+//   <div>This is div</div>
+//   <p>this is o</p>
+// `
 
-// const stringInt = '40'
-// const stringFloat = '40.42'
-// console.log(Number.parseInt(stringInt) + 2)
-// console.log(parseInt(stringInt) + 2)
-// console.log(Number(stringInt) + 2)
-// console.log(+stringInt + 2)
-// console.log(parseFloat(stringFloat) + 2)
-// console.log(+stringFloat + 2)
+// console.log(output)
 
-// console.log(0.4 + 0.2) // 0.6
-// console.log(+(0.4 + 0.2).toFixed(1))
-// console.log(parseFloat((0.4 + 0.2).toFixed(1)))
+// const name = 'Ira'
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.charAt(2))
+// console.log(name.indexOf('!'))
+// console.log(name.toLowerCase().startsWith('Ira'))
+// console.log(name.startsWith('Ira'))
+// console.log(name.endsWith('ra!'))
+// console.log(name.repeat(3))
+// const string = '     password      '
+// console.log(string.trim())
+// console.log(string.trimLeft())
+// console.log(string.trimRight())
 
-// BigInt
-// console.log(90071992547409919999999n - 9007199254740991999999n)
-// console.log(-90071992547409919999999n)
-// console.log(90071992547409919999999.23231n) // error
-
-// console.log(10n - 4) // error
-// console.log(parseInt(10n) - 4)
-// console.log(10n - BigInt(4))
-// console.log(5n / 2n) // 2n
-
-// 3 Math
-// console.log(Math.E)
-// console.log(Math.PI)
-
-// console.log(Math.sqrt(25))
-// console.log(Math.pow(5, 3))
-// console.log(Math.abs(-42))
-// console.log(Math.max(42, 12, 23, 11, 420))
-// console.log(Math.min(42, 12, 23, 11, 420))
-// console.log(Math.floor(4.9))
-// console.log(Math.ceil(4.9))
-// console.log(Math.round(4.4))
-// console.log(Math.trunc(4.9))
-// console.log(Math.random())
-
-// 4 Example
-function getRandomBetween(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+function logPerson(s, name, age) {
+    if (age < 0) {
+      age = 'Еще не родился'
+    }
+    return `${s[0]}${name}${s[1]}${age}${s[2]}`
   }
   
-  console.log(getRandomBetween(10, 42))
+  const personName = 'Ira'
+  const personName2 = 'Masha'
+  const personAge = 19
+  const personAge2 = -10
+  
+  const output = logPerson`Имя: ${personName}, Возраст: ${personAge}!`
+  const output2 = logPerson`Имя: ${personName2}, Возраст: ${personAge2}!`
+  
+  console.log(output)
+  console.log(output2)
