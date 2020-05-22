ffd0039b106b4d0a99fcdf8242ab019466726111
Ira <ilovebellsdaya@yandex.ru>
2020-05-22 21:58:04 +0300

    операторы

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 // 1 Переменные
 // camelCase - 2nd word with uppercase
 
-const firstName = 'Ira'
-let age = 19 //number
-const isProgrammer = true //Boolean
+// const firstName = 'Ira'
+// let age = 19 //number
+// const isProgrammer = true //Boolean
 
-const private = 'private'
-const $ = 'some value'
+// const private = 'private'
+// const $ = 'some value'
 
-const withNum5 = '5'
+// const withNum5 = '5'
 //const 5withNum5 = 5 //err
 
 // 2 Мутирование
 //Console.log('Name: ' + firstName + ' , age: ' + age)
 //alert('Name: ' + firstName + ' , age: ' + age)
 
-const lastName = prompt( message: 'Введите фамилию')
-alert(firstName + ' ' + lastName)
+// const lastName = prompt( message: 'Введите фамилию')
+// alert(firstName + ' ' + lastName)
+
+// 3 Операторы
+// let currentYear = 2020
+// const birthYear = 2000
+
+// const age = currentYear - birthYear
+
+// let c = 32
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// Console.log(a + b)
+// Console.log(a - b)
+// Console.log(a * b)
+// Console.log(a / b)
+// Console.log(++currentYear)
+// Console.log(--currentYear)
+// Console.log(c)
