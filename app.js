dc269dc740a12f20f02b3012162d7b4588dd09aa
Ira <ilovebellsdaya@yandex.ru>
2020-05-23 12:26:55 +0300

    object

--- app.js
+++ app.js
@@ -1,84 +1,89 @@
-const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседс']
-// const people = [
-//   {name: 'Ira', budget: 4200},
-//   {name: 'Masha', budget: 3500},
-//   {name: 'Victoria', budget: 1700}
-// ]
-const fib = [1, 1, 2, 3, 5, 8, 13]
-
-// Function
-function addItemToEnd() {
-
-}
-
-// Method
-// cars.push('Рено')
-// cars.unshift('Волга')
-
-// const firstCar = cars.shift()
-// const lastCar = cars.pop()
-// console.log(firstCar)
-// console.log(lastCar)
-// console.log(cars)
-
-// console.log(cars.reverse())
-// console.log(cars)
-
-// const index = cars.indexOf('БМВ')
-// cars[index] = 'Porsche'
-// console.log(cars)
-// let findedPerson
-// for (const person of people) {
-//   if (person.budget === 3500) {
-//     findedPerson = person
-//   }
-// }
-
-// console.log(findedPerson)
-
-// const index = people.findIndex(function(person) {
-//   return person.budget === 3500
-// })
-// const person = people.find(function(person) {
-//   return person.budget === 3500
-// })
-// console.log(person)
-// const person = people.find(person => person.budget === 3500)
-// console.log(person)
-
-// console.log(cars.includes('Мазда!'))
-
-// const upperCaseCars = cars.map(car => {
-//   return car.toUpperCase()
-// })
-
-const pow2 = num => num ** 2
-// const sqrt = num => Math.sqrt(num)
-
-// const pow2Fib = fib.map(pow2).map(Math.sqrt)
-// console.log(pow2Fib)
-// const pow2Fib = fib.map(pow2)
-// const filteredNumbers = pow2Fib.filter(num => num > 20)
-// console.log(pow2Fib)
-// console.log(filteredNumbers)
-
-// Задача 1
-// const text = 'Привет, мы изучаем JavaScript'
-// const reverseText = text.split('').reverse().join('')
-// console.log(reverseText)
-
-const people = [
-    {name: 'Ira', budget: 4200},
-    {name: 'Masha', budget: 3500},
-    {name: 'Victoria', budget: 1700}
-  ]
+const person = {
+    name: 'Ira',
+    age: 19,
+    isProgrammer: true,
+    languages: ['ru', 'en', 'de'],
+    // 'complex key': 'Complex Value',
+    // ['key_' + (1 + 3)]: 'Computed Key', // key_4
+    greet() {
+      console.log('greet from person')
+    },
+    info() {
+      // console.log('this:', this)
+      console.info('Информация про человека по имени:', this.name)
+    }
+  }
   
-  const allBudget = people
-    .filter(person => person.budget > 2000)
-    .reduce((acc, person) => {
-      acc += person.budget
-      return acc
-    }, 0)
+  // console.log(person.name)
+  // const ageKey = 'age'
+  // console.log(person[ageKey])
+  // console.log(person['complex key'])
+  // person.greet()
   
-  console.log(allBudget)
-  // const displayItems = allItems.filter(item => item.name.indexOf('phone') !== -1)
+  // person.age++
+  // person.languages.push('by')
+  // // person['key_4'] = undefined
+  // delete person['key_4']
+  //
+  // console.log(person)
+  // console.log(person['key_4'])
+  
+  // const name = person.name
+  // const age = person.age
+  // const languages = person.languages
+  
+  // const {name, age: personAge = 10, languages} = person
+  // console.log(person)
+  
+  // for (let key in person) {
+  //   if (person.hasOwnProperty(key)) {
+  //     console.log('key:', key)
+  //     console.log('value:', person[key])
+  //   }
+  // }
+  // Object.keys(person).forEach((key) => {
+  //   console.log('key:', key)
+  //   console.log('value:', person[key])
+  // })
+  
+  // Context
+  // person.info()
+  
+  const logger = {
+    keys() {
+      console.log('Object Keys: ', Object.keys(this))
+    },
+  
+    keysAndValues() {
+      // "key": value
+      // Object.keys(this).forEach(key => {
+      //   console.log(`"${key}": ${this[key]}`)
+      // })
+      // const self = this
+      Object.keys(this).forEach(function(key) {
+        console.log(`"${key}": ${this[key]}`)
+      }.bind(this))
+    },
+  
+    withParams(top = false, between = false, bottom = false) {
+      if (top) {
+        console.log('------- Start -------')
+      }
+      Object.keys(this).forEach((key, index, array) => {
+        console.log(`"${key}": ${this[key]}`)
+        if (between && index !== array.length - 1) {
+          console.log('--------------')
+        }
+      })
+  
+      if (bottom) {
+        console.log('------- End -------')
+      }
+    }
+  }
+  
+  // const bound = logger.keys.bind(person)
+  // bound()
+  // logger.keysAndValues.call(person)
+  logger.withParams.call(person, true, true, true)
+  logger.withParams.apply(person, [true, true, true])
